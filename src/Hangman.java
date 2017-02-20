2eb9825ef6f8c9e573f313487ef1f32da956fbe5
Andrew Harris <blakebharris@gmail.com>
2017-02-20 17:02:34 -0600

    uptodate

--- src/Hangman.java
+++ src/Hangman.java
@@ -2,25 +2,58 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Hangman {
-    private String wordAnswer;  //Solution
-    private String secretWord ="";  //Current word will include _
+    private String wordAnswer;
+    private String secretWord ="";
     private int numberGuesses = 0;
     private String lettersGuessed = "";
 
     public void printInstructions(){
-//        System.out.println("The man is on the gallows\nthe only way to save him\nis to guess the word in front of you\n" +
-//                "and you only have 6 tries!");
+        System.out.println("The man is on the gallows\nthe only way to save him\nis to guess the word in front of you\n" +
+                "and you only have 6 tries!");
+    }
+
+    public String getLettersGuessed(){
+        return lettersGuessed;
+    }
+
+    public int getNumberGuesses(){
+        return numberGuesses;
+    }
+
+    public void setWordAnswer(String wordAnswer){
+        this.wordAnswer = wordAnswer;
+    }
+
+    public void setLettersGuessed(String s){
+        this.lettersGuessed = s;
     }
 
     public void play(){
         Scanner scanner = new Scanner(System.in);
-        while(numberGuesses < 6 || wordAnswer.equals(secretWord)) {
+        wordAnswer = setWord();
+        while(numberGuesses < 6 ) {
             System.out.printf("choose a letter: ");
-            String userInput = scanner.nextLine();
+            String userInput = scanner.nextLine().toLowerCase();
             guessLetter(userInput);
             this.secretWord = hiddenWord();
             printStanding();
+            if(this.secretWord.equals(wordAnswer)) {
+                break;
+            }
         }
+        if(numberGuesses == 6) {
+            System.out.printf( "Sorry, out of guesses, the word was: '%s'", wordAnswer);
+        }else {
+            System.out.println("great job, you guessed it!");
+        }
+    }
+
+    public void promptUser(){
+
+    }
+
+    public String getWordAnswer(){
+        return this.wordAnswer;
     }
 
     public void printStanding(){
@@ -29,12 +62,13 @@ public class Hangman {
         System.out.println("To Guess: " + secretWord);
     }
 
-    public void setWord(){
+    public String setWord(){
         String[] listOfWords = {
-                "dog", "cat", "elmo", "easy", "apple",
-                "caterpillar", "eskimo", "origami", "sushi"
+                "dog", "cat", "elmo", "water", "alaska",
+                "thermal", "inadvertently", "purified",
+                "eating", "forward", "drowning", "career"
         };
-        wordAnswer = listOfWords[getRandomNumber(listOfWords)];
+        return listOfWords[getRandomNumber(listOfWords)];
     }
 
     public int getRandomNumber(String[] arry){
@@ -47,6 +81,7 @@ public class Hangman {
             return;
         } else if (!this.wordAnswer.contains(letter)){
             numberGuesses++;
+            this.lettersGuessed += letter;
         } else {
             this.lettersGuessed += letter;
         }
--- tests/HangmanTest.java
+++ tests/HangmanTest.java
@@ -1,7 +1,99 @@
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.contrib.java.lang.system.SystemOutRule;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class HangmanTest {
-    @Test
 
+    @Test
+    public void getRandomNum(){
+        Hangman h = new Hangman();
+        String[] listOfWords = {
+                "dog", "cat", "elmo", "easy", "apple",
+                "caterpillar", "eskimo", "origami", "sushi"
+        };
+
+        int i = 0;
+        while (i < 100) {
+            assertTrue(h.getRandomNumber(listOfWords) < listOfWords.length);
+            i++;
+        }
+    }
+
+    @Test
+    public void setARandomWord(){
+        Hangman h = new Hangman();
+        String word = h.setWord();
+        String[] listOfWords = {
+                "dog", "cat", "elmo", "easy", "apple",
+                "caterpillar", "eskimo", "origami", "sushi"
+        };
+
+        assertTrue(Arrays.asList(listOfWords).contains(word));
+
+    }
+
+    @Test
+    public void hiddenWord(){
+        Hangman h = new Hangman();
+        h.setWordAnswer("hello");
+        h.setLettersGuessed("ho");
+
+        assertTrue(h.hiddenWord().equals("h___o"));
+    }
+
+    @Test
+    public void checkNumberOfGuesses(){
+        Hangman h = new Hangman();
+        h.setWordAnswer("hello");
+        h.guessLetter("x");
+        h.guessLetter("t");
+        h.guessLetter("e"); //in the wordAnswer so doesn't increment numberOfGuesses.
+        assertTrue(h.getNumberGuesses() == 2);
+    }
+
+    @Test
+    public void getLettersGuesses(){
+        Hangman h = new Hangman();
+        h.setWordAnswer("hello");
+        h.guessLetter("x");
+        h.guessLetter("t");
+        h.guessLetter("e"); //in the wordAnswer so doesn't increment numberOfGuesses.
+        assertTrue(h.getLettersGuessed().equals("xte"));
+    }
+
+    @Test
+    public void playGuessesExceeded(){
+        Hangman h = new Hangman();
+        h.setWordAnswer("hello");
+        h.guessLetter("x");
+        h.guessLetter("t");
+        h.guessLetter("m");
+        h.guessLetter("j");
+        h.guessLetter("a");
+        h.guessLetter("r");
+        assertTrue(h.getNumberGuesses() > 5);
+    }
+
+    @Rule
+    public final SystemOutRule systemOutRule = new SystemOutRule().enableLog();
+
+    @Test
+    public void x() {
+        Hangman h = new Hangman();
+        h.setWordAnswer("hello");
+        h.guessLetter("x");
+        h.guessLetter("t");
+        h.guessLetter("m");
+        h.guessLetter("j");
+        h.guessLetter("a");
+        h.guessLetter("r");
+        h.play();
+        assertEquals(String.format("Sorry, out of guesses, the word was: '%s'",h.getWordAnswer()) , systemOutRule.getLog());
+    }
 
 }
